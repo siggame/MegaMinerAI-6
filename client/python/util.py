826b442fd35c48f7dd37c4a67fbe3181ed9ea41b
David Norton <david.k.norton@gmail.com>
2010-11-05 16:47:50 -0500

    Fixed Doxygen comment for distance function in python client.

--- client/python/util.py
+++ client/python/util.py
@@ -1,9 +1,8 @@
 # -*- coding: iso-8859-1 -*-
 import library
 
-///
-///Given the x,y and size of two bots, returns the Manhattan distance
-///
+
+##Given the x,y and size of two bots, returns the Manhattan distance
 def distance(fromX, fromY, fromSize, toX, toY, toSize):
   return library.distance(fromX, fromY, fromSize, toX, toY, toSize)
   
