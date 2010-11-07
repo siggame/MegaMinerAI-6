f45608a4c4a5e218907e8d936e0ed45b6c24ade7
mst00110 <mst00110@webserver.(none)>
2010-11-06 21:39:32 -0500

    Lots of stuff that happend while Wisley was gone! Yay!

--- arena/db/visched.py
+++ arena/db/visched.py
@@ -4,7 +4,7 @@ import threading
 
 
 class GameRecord:
-  def __init__(priority, p1, p2, ver1, ver2,log):
+  def __init__(self,priority, p1, p2, ver1, ver2,log):
     self.priority = priority
     self.p1 = p1
     self.p2 = p2
@@ -21,10 +21,14 @@ class VisScheduler:
     self.lock = threading.Semaphore()
     
   def visualizerQueueUpdate(self, game):
+    print game
+    print self.lock
     with self.lock:
+      print "Semaphore Works!"
       found = False
       for record in self.visq:
       #finds the previous game between the same players in the same player numbers
+        print game.p1,record.p1
         if game.p1 == record.p1 and game.p2 == record.p2:
           found = True
           #updates the gamelog stored
@@ -32,18 +36,19 @@ class VisScheduler:
           #Checks if its a new version
           if game.p1ver != record.p1ver or game.p2ver != record.p2ver:
             #if so, it updates the version and increases the priority (new games are more interesting than old games!)
-            record.p1ver = newgame.p1ver
-            record.p2ver = newgame.p2ver
-            record.priority += 2*self.visq.len()
+            record.p1ver = game.p1ver
+            record.p2ver = game.p2ver
+            record.priority += 2*len(self.visq)
       #If the game holder between those players doesn't already exist, it creates it
       if not found:
-        game.priority = 2*self.visq.len() + 2
+        game.priority = 2*len(self.visq) + 2
         self.visq.append(game)
     return True
   
   #grabs the next thing to be visualized and makes proper adjustments
   def nextVideo(self):
     with self.lock:
+      print self.visq
       if len(self.visq) > 0:
         self.visq.sort(key = lambda x: x.priority, reverse=True)
         nextUP = self.visq[0]
