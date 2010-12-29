e8a6e0bb3f9034343a91c34ef9c14a7e63844b48
David Norton <david.k.norton@gmail.com>
2010-12-29 17:49:19 -0600

    Added build invocator info.

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# Give build info
+echo -n "Build started by " > build.log # -n flag drops ending newline
+whoami | tr -d "\n" >> build.log # truncates newline char from whoami
+echo -n " at " >> build.log
+date >> build.log
+
+exit 0
+
+
