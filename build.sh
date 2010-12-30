3317df1bc374295d3a41c0697e43b6005941f782
David Norton <david.k.norton@gmail.com>
2010-12-29 21:49:04 -0600

    Add a newline at the end of the first line.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ echo -n "Build started by " > build.log # -n flag drops ending newline
 whoami | tr -d "\n" >> build.log # truncates newline char from whoami
 echo -n " at " >> build.log
 date >> build.log
-
+echo "" >> build.log # Throw a newline on the end...
 exit 0
 
 
