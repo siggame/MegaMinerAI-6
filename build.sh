a22d580da6cf3a053df2117bb390b00187eefc1c
David Norton <david.k.norton@gmail.com>
2010-12-31 17:23:04 -0600

    Removed hardcoded filepaths, replacing them with variables.

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
+logfilename=build.log # name of log
+rootdir=$PWD # where this script is located
+logfilepath=$rootdir/$logfilename
+
 # Give build info
-echo -n "Build started by " | tee build.log # -n flag drops ending newline
-whoami | tr -d "\n" | tee -a build.log # truncates newline char from whoami
-echo -n " at " | tee -a build.log
-date | tee -a build.log
-echo "" | tee -a build.log # Throw a newline on the end...
+echo -n "Build started by " | tee $logfilepath # -n flag drops ending newline
+whoami | tr -d "\n" | tee -a $logfilepath # truncates newline char from whoami
+echo -n " at " | tee -a $logfilepath
+date | tee -a $logfilepath
+echo "" | tee -a $logfilepath # Throw a newline on the end...
 
 # Start building!
 
-
-
-echo "Starting parser build..." | tee -a build.log
-echo "Moving to parser directory..." | tee -a build.log
-cd ./visualizer/parser/
-echo "Building parser..." | tee -a build.log
-make |& tee -a ../../build.log # |& pipes sterr AND stout to tee
+echo "Starting parser build..." | tee -a $logfilepath
+cd ./visualizer/parser
+make |& tee -a $logfilepath # |& pipes sterr AND stout to tee
 
 exit 0
 
