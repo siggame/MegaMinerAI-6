0353b7b74f3f47b15d8081857b1e5075d8967a67
David Norton <david.k.norton@gmail.com>
2010-12-31 14:11:16 -0600

    Changed direct logfile write with tee arguments so that it shows up on the screen as well.

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@
 
 # Give build info
 echo -n "Build started by " | tee build.log # -n flag drops ending newline
-whoami | tr -d "\n" >> build.log # truncates newline char from whoami
-echo -n " at " >> build.log
-date >> build.log
-echo "" >> build.log # Throw a newline on the end...
+whoami | tr -d "\n" | tee -a build.log # truncates newline char from whoami
+echo -n " at " | tee -a build.log
+date | tee -a build.log
+echo "" | tee -a build.log # Throw a newline on the end...
 
 # Start building!
 
 
-echo "--- Starting Parser Build --" >> build.log
-echo "cd'ing to ./" 
+echo "Starting Parser build..." | tee -a build.log
+echo "Moving to parser directory..." | tee -a build.log
 
 exit 0
 
